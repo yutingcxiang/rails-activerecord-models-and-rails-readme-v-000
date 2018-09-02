b3c40f3760996a208ea1508910e2c41c509aa1a3
Christine Xiang <yutingcxiang@gmail.com>
2018-09-02 07:51:31 +0000

    automatically backed up by learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,4 +5,9 @@ describe Post do
     post = Post.create!(title: "My title", description: "The post description")
     expect(post).to be_valid
   end
+  
+  it 'has a summary' do
+  post = Post.create!(title: "My title", description: "The post description")
+  expect(post.post_summary).to eq("My title - The post description")
+  end
 end
